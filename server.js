05552de1041758faf4e681a06c514db481d8053d
decentninja <very@decent.ninja>
2016-01-21 23:28:18 +0100

    Easier server.js

--- server.js
+++ server.js
@@ -7,30 +7,24 @@ var browserify = require("browserify");
 var babelify = require("babelify");
 var Bawang = require("./components/bawang/bawang.jsx");
 var translateServer = require("./components/translate/server.js");
-var fs = require("fs");
+var path = require('path');
 
 
 var app = express();
 app.use(cookieParser());
 
 var bundle = null;
-var site = "";
 
-Promise.all([
-    new Promise((resolve, reject) => {
-        var b = browserify();
-        b.add("./components/bawang/bawang.jsx");
-        b.transform(babelify).bundle(function(err, buf) {
-            if(err) console.error(err);
-            bundle = buf;
-            resolve();
-        });
-    }),
-    new Promise((resolve, reject) => fs.readFile("./index.html", (err, data) => {
-        site = data;
-        resolve();
-    }))
-]).then(() => {
+function build_client(callback) {
+    var b = browserify();
+    b.add("./components/bawang/bawang.jsx");
+    b.transform(babelify).bundle(function(err, buf) {
+        if(err) console.error(err);
+        bundle = buf;
+        callback();
+    });
+}
+build_client(() => {
     app.listen(PORT);
     console.log("Listening on", PORT);
 });
@@ -45,5 +39,5 @@ app.use('/components/', express.static('components'));
 app.get("/*", function(req, res) {
     translateServer.setlang(res, req);
     res.type('html');
-    res.send(site);
+    res.sendFile(path.resolve("./index.html"));
 });
